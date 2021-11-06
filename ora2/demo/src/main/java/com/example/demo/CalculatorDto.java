6debaf5eff8d71f9fff7543e4bb22b3eaf593fd8
Balázs Mura-Mészáros <mura.m.balazs@gmail.com>
2021-11-06 13:09:51 +0100

    .

---
+++ ora2/demo/src/main/java/com/example/demo/CalculatorDto.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+public class CalculatorDto {
+	
+	private int a;
+	private int b;
+	private String operator;
+	
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	public int getB() {
+		return b;
+	}
+	public void setB(int b) {
+		this.b = b;
+	}
+	public String getOperator() {
+		return operator;
+	}
+	public void setOperator(String operator) {
+		this.operator = operator;
+	}
+	
+	
+	
+}
