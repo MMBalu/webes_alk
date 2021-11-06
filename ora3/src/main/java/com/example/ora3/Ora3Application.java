705aa67ffa862dd09243aac6686ae4b4b7ac70fe
Balázs Mura-Mészáros <mura.m.balazs@gmail.com>
2021-11-06 09:39:00 +0100

    3. óra első feltöltés

--- ora3/src/main/java/com/example/ora3/Ora3Application.java
+++ ora3/src/main/java/com/example/ora3/Ora3Application.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.ora3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Ora3Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Ora3Application.class, args);
 	}
 
 }
--- ora3/src/test/java/com/example/ora3/Ora3ApplicationTests.java
+++ ora3/src/test/java/com/example/ora3/Ora3ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.ora3;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class Ora3ApplicationTests {
 
 	@Test
 	void contextLoads() {
